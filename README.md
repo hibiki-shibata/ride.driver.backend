f493f79d7ef7c264f021054a80a0818a6515750d
hibiki-shibata <hibikishibata1853@gmail.com>
2025-07-26 23:59:05 +0900

    readme.md added

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a backend for Ride Driver App
--- app/src/main/kotlin/com/ride/driver/backend/RideDriverBackendApplication.kt
+++ app/src/main/kotlin/com/ride/driver/backend/RideDriverBackendApplication.kt
@@ -3,10 +3,10 @@ package com.ride.driver.backend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-// @SpringBootApplication
-// class RideDriverBackendApplication
+@SpringBootApplication
+class RideDriverBackendApplication
 
 fun main(args: Array<String>) {
 	println("Ride Driver Backend Application is running!")
-	// runApplication<RideDriverBackendApplication>(*args)
+	runApplication<RideDriverBackendApplication>(*args)
 }
