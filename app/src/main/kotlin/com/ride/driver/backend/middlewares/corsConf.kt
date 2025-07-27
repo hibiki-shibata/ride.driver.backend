18f5c03649b0f672380fad0e7c3e7dafa5ddd549
hibiki-shibata <hibikishibata1853@gmail.com>
2025-07-28 06:44:04 +0900

    cors global config added

--- app/src/main/kotlin/com/ride/driver/backend/middlewares/corsConf.kt
+++ app/src/main/kotlin/com/ride/driver/backend/middlewares/corsConf.kt
@@ -1,1 +1,17 @@
+// Doc https://spring.io/guides/gs/rest-service-cors#global-cors-configuration
 package com.ride.driver.backend.middlewares
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurator
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfiguration
+
+open class CorsConf: WebMvcConfigurator{
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "https://example.com")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
