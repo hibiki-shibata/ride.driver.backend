56085e5363800c7232675c93bb8309f3e8caadef
hibiki-shibata <hibiki.shibata@wolt.com>
2026-03-12 02:20:40 +0900

    Add: consumerProfile controller

--- app-main/src/main/kotlin/com/ride/driver/backend/controllers/consumerProfileController.kt
+++ app-main/src/main/kotlin/com/ride/driver/backend/controllers/consumerProfileController.kt
@@ -1,54 +1,43 @@
-// package com.ride.driver.backend.controller
+package com.ride.driver.backend.controller
 
-// import org.springframework.web.bind.annotation.GetMapping
-// import org.springframework.web.bind.annotation.RequestMapping
-// import org.springframework.web.bind.annotation.RestController
-// import org.springframework.web.bind.annotation.RequestParam
-// import org.springframework.web.bind.annotation.RequestBody
-// import org.springframework.web.bind.annotation.RequestHeader
-// import org.springframework.http.ResponseEntity
-// import org.springframework.security.core.context.SecurityContextHolder
-// import org.springframework.data.domain.Page
-// import org.springframework.data.domain.Pageable
-// import com.ride.driver.backend.repositories.CourierProfileRepository
-// import com.ride.driver.backend.models.courierProfile.CourierProfile
-// import com.ride.driver.backend.models.courierProfile.OperationArea
-// import com.ride.driver.backend.models.courierProfile.VehicleType
-// import com.ride.driver.backend.models.courierProfile.CourierStatus
-// import com.ride.driver.backend.services.AccessTokenData
-// import java.util.UUID
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import com.ride.driver.backend.repositories.ConsumerProfileRepository
+import com.ride.driver.backend.models.consumerProfile.ConsumerProfile
+import com.ride.driver.backend.services.AccessTokenData
+import java.util.UUID
 
-// data class ConsumerProfileDTO(
-//     val id: UUID?,
-//     val name: String,
-//     val phoneNumber: String,
-//     val vehicleType: VehicleType?,
-//     val rate: Double?,
-//     val status: CourierStatus,
-//     val operationArea: OperationArea?,
-//     val comments: String?
-// )
+data class ConsumerProfileDTO(
+    val id: UUID?,
+    val name: String,
+    val emailAddress: String,
+)
 
-// @RestController
-// @RequestMapping("api/v1/couriers")
-// class CourierProfileController (   
-//     private val repository: CourierProfileRepository,
-// ){
-//     @GetMapping("/courier/me")
-//     fun findCourierProfile(): ResponseEntity<CourierProfileDTO> {        
-//         println("Finding all couriers...")
-//         val courierDetails: AccessTokenData = SecurityContextHolder.getContext().authentication?.principal as AccessTokenData ?: return ResponseEntity.status(401).build()
-//         val courierId: UUID = courierDetails.additonalClaims.courierId
-//         val courier: CourierProfile = repository.findById(courierId) ?: throw Exception("Courier not found with ID: $courierId")
-//         val courierDTO = CourierProfileDTO(
-//             id = courier.id,
-//             name = courier.cpFirstName + " " + courier.cpLastName,
-//             phoneNumber = courier.phoneNumber,
-//             vehicleType = courier.vehicleType,
-//             rate = courier.cpRate,
-//             status = courier.cpStatus,
-//             operationArea = courier.operationArea,
-//             comments = courier.cpComments
-//         )
-//         return ResponseEntity.ok(courierDTO)
-//     }
+@RestController
+@RequestMapping("api/v1/consumers")
+class ConsumerProfileController (   
+    private val consumerProfileRepository: ConsumerProfileRepository,
+){
+    @GetMapping("/consumer/me")
+    fun findConsumerProfile(): ResponseEntity<ConsumerProfileDTO> {        
+        println("Finding consumer profile...")
+        val consumerDetails: AccessTokenData = SecurityContextHolder.getContext().authentication?.principal as AccessTokenData ?: return ResponseEntity.status(401).build()
+        val consumerId: UUID = consumerDetails.additonalClaims.accountID
+        val consumer: ConsumerProfile = consumerProfileRepository.findById(consumerId) ?: throw Exception("Consumer not found with ID: $consumerId")
+        return ResponseEntity.ok(
+            ConsumerProfileDTO(
+            id = consumer.id,
+            name = consumer.cxFirstName + " " + consumer.cxLastName,
+            emailAddress = consumer.emailAddress
+         )
+        )
+    }
+}
