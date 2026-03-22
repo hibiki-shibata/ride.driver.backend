baf2f9aa520cbf03670cf4109028483ad6cbc771
hibiki-shibata <hibiki.shibata@wolt.com>
2026-03-23 03:46:03 +0900

    Add: transacation for updating consumer profile

--- app-main/src/main/kotlin/com/ride/driver/backend/consumer/repository/consumerProfileRepo.kt
+++ app-main/src/main/kotlin/com/ride/driver/backend/consumer/repository/consumerProfileRepo.kt
@@ -1,18 +1,32 @@
-// https://spring.io/projects/spring-data-jpa
+// // https://spring.io/projects/spring-data-jpa
+// package com.ride.driver.backend.consumer.repository
+
+// import org.springframework.data.repository.CrudRepository
+// import org.springframework.data.domain.Page
+// import org.springframework.data.domain.Pageable
+// import org.springframework.stereotype.Repository
+// import java.util.UUID
+// import com.ride.driver.backend.consumer.model.ConsumerProfile
+
+// @Repository
+// open interface ConsumerProfileRepository : CrudRepository<ConsumerProfile, Long> {
+//    fun save(consumerProfile: ConsumerProfile): ConsumerProfile
+//    fun existsByEmailAddress(emailAddress: String): Boolean
+//    fun findById(id: UUID): ConsumerProfile
+//    fun findByEmailAddress(emailAddress: String): ConsumerProfile
+//    fun findAll(pageable: Pageable): Page<ConsumerProfile>
+// }
+
 package com.ride.driver.backend.consumer.repository
 
-import org.springframework.data.repository.CrudRepository
+import com.ride.driver.backend.consumer.model.ConsumerProfile
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.stereotype.Repository
+import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
-import com.ride.driver.backend.consumer.model.ConsumerProfile
 
-@Repository
-open interface ConsumerProfileRepository : CrudRepository<ConsumerProfile, Long> {
-   fun save(consumerProfile: ConsumerProfile): ConsumerProfile
-   fun existsByEmailAddress(emailAddress: String): Boolean
-   fun findById(id: UUID): ConsumerProfile
-   fun findByEmailAddress(emailAddress: String): ConsumerProfile
-   fun findAll(pageable: Pageable): Page<ConsumerProfile>
+interface ConsumerProfileRepository : JpaRepository<ConsumerProfile, UUID> {
+    fun existsByEmailAddress(emailAddress: String): Boolean
+    fun findByEmailAddress(emailAddress: String): ConsumerProfile?
+    fun findByEmailAddressIgnoreCase(emailAddress: String): ConsumerProfile?
 }
