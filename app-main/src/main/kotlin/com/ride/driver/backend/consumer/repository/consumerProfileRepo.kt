681499420895d1a36e8e5165d195d90c4b29dbf3
hibiki-shibata <hibiki.shibata@wolt.com>
2026-03-23 05:34:24 +0900

    Comment out repo

--- app-main/src/main/kotlin/com/ride/driver/backend/consumer/repository/consumerProfileRepo.kt
+++ app-main/src/main/kotlin/com/ride/driver/backend/consumer/repository/consumerProfileRepo.kt
@@ -30,4 +30,13 @@ interface ConsumerProfileRepository : JpaRepository<ConsumerProfile, UUID> {
     fun existsByEmailAddress(emailAddress: String): Boolean
     fun findByEmailAddress(emailAddress: String): ConsumerProfile?
     fun findByEmailAddressIgnoreCase(emailAddress: String): ConsumerProfile?
+   //  fun findAllByEmailAddress(emailAddress: String, pageable: Pageable): Page<ConsumerProfile>
 }
+
+
+// import org.springframework.data.domain.Page
+// import org.springframework.data.domain.Pageable
+// import org.springframework.data.domain.PageRequest
+
+// val pageRequest = PageRequest.of(0, 20) // page 0, size 20
+// val pagePage<ConsumerProfile> = consumerProfileRepository.findAll(pageRequest)
