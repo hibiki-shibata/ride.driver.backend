0e84855985db97a7dfa0594c05db859e6f2aa400
hibiki-shibata <hibiki.shibata@wolt.com>
2026-03-27 00:21:36 +0900

    Moduled: refreshTokenService

--- app-main/src/main/kotlin/com/ride/driver/backend/shared/auth/controller/authController.kt
+++ app-main/src/main/kotlin/com/ride/driver/backend/shared/auth/controller/authController.kt
@@ -1,35 +1,19 @@
 package com.ride.driver.backend.shared.auth.controller
 
-import java.util.UUID
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import com.ride.driver.backend.shared.auth.service.JwtTokenService
-import com.ride.driver.backend.shared.auth.domain.AccessTokenClaim
-import com.ride.driver.backend.shared.auth.domain.AccountRoles
+import com.ride.driver.backend.shared.auth.service.RefreshTokenService
 import com.ride.driver.backend.shared.auth.dto.JwtTokensDTO
-import com.ride.driver.backend.shared.exception.InvalidJwtTokenException
 
 @RestController
 @RequestMapping("/api/v1/auth")
 class AuthController(
-    private val jwtTokenService: JwtTokenService,
+    private val refreshTokenService: RefreshTokenService,
 ) {
     @PostMapping("/refresh-token")
     fun refreshToken(@RequestBody @Valid req: JwtTokensDTO): ResponseEntity<JwtTokensDTO> {
-        val accountIdOfRefreshToken: UUID = jwtTokenService.extractAccountId(req.refreshToken)
-        val acccountIdOfAccessToken: UUID = jwtTokenService.extractAccountId(req.accessToken)
-        if (accountIdOfRefreshToken != acccountIdOfAccessToken) throw InvalidJwtTokenException("The access token and refresh token does not belong to the same account")
-        if (!jwtTokenService.isTokenValid(req.refreshToken)) throw InvalidJwtTokenException("Refresh token is either expired or invalid")
-        val accountName: String = jwtTokenService.extractAccountName(req.accessToken)
-        val newAccessToken: String = jwtTokenService.generateAccessToken(
-            AccessTokenClaim(
-                accountId = accountIdOfRefreshToken,
-                accountName = accountName,
-                accountRoles = listOf(AccountRoles.BASE_CONSUMER_ROLE)
-            )
-        )
-        val newRefreshToken: String = jwtTokenService.generateRefreshToken(accountIdOfRefreshToken)
-        return ResponseEntity.ok(JwtTokensDTO(newAccessToken, newRefreshToken))
+        val refreshedJwtTokens: JwtTokensDTO = refreshTokenService.updateRefreshToken(req)
+        return ResponseEntity.ok(refreshedJwtTokens)
     }
 }
