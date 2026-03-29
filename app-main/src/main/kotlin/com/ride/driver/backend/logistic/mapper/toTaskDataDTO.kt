f4822897d27c87d94dc582f6386b2a248357def6
hibiki-shibata <hibiki.shibata@wolt.com>
2026-03-29 14:44:16 -0700

    Add: toTaskDataDto

---
+++ app-main/src/main/kotlin/com/ride/driver/backend/logistic/mapper/toTaskDataDTO.kt
@@ -0,0 +1,20 @@
+package com.ride.driver.backend.logistic.mapper
+
+import com.ride.driver.backend.logistic.dto.TaskDataDTO
+import com.ride.driver.backend.logistic.model.Task
+
+fun Task.toTaskDataDTO(): TaskDataDTO {
+    return TaskDataDTO(
+        taskId = id.toString(),
+        consumerName = consumerProfile?.name ?: "Unknown Consumer",
+        consumerEmailaddress = consumerProfile?.emailAddress ?: "Unknown Phone Number",
+        pickupAddress = merchantProfile?.merchantAddress ?: "Unknown Pickup Address",
+        pickupLatitude = merchantProfile?.merchantAddressCoordinate?.latitude ?: 0.0,
+        pickupLongitude = merchantProfile?.merchantAddressCoordinate?.longitude ?: 0.0,
+        dropoffAddress = consumerProfile?.consumerAddress ?: "Unknown Dropoff Address",
+        dropoffLatitude = consumerProfile?.consumerAddressCoordinate?.latitude ?: 0.0,
+        dropoffLongitude = consumerProfile?.consumerAddressCoordinate?.longitude ?: 0.0,
+        itemNames = orderedItems.map { it.name },
+        totalPrice = totalPrice
+    )
+}
